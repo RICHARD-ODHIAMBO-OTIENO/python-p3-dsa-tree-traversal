8c4c17651689bf0254d474fabab1372db43241c4
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-07-24 19:31:43 +0300

    DSA-Tree-Traversal Final Commit

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,23 @@
-class Tree:
-  def __init__(self, root = None):
-    self.root = root
+# Initialize an empty output list
+# Initialize an list of nodes to visit and add the root node to it
+# While there are nodes in the nodes to visit list
+#   Remove the first node from the nodes to visit list
+#   Add its value to the output list
+#   Add its children (if any) to the end of the nodes to visit list
+# Return the output list
 
-  def get_element_by_id(self, id):
-    pass
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def get_element_by_id(self, id):
+        return self._depth_first_search(self.root, id)
+
+    def _depth_first_search(self, node, id):
+        if node['id'] == id:
+            return node
+        for child in node['children']:
+            result = self._depth_first_search(child, id)
+            if result:
+                return result
+        return None
